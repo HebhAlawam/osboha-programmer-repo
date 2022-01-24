f9891de32c21a4cdd2725524dab8cc28776b309c
RamaHz <98233623+RamaHz@users.noreply.github.com>
2022-01-24 09:31:26 -0800

    Update and rename john-doe.blade.php to Rama.blade.php

---
+++ resources/views/Rama.blade.php
@@ -0,0 +1,106 @@
+<!-- From New Branch -->
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+a:link, a:visited{
+background-color:powderblue;
+padding:10px;
+color:black;
+text-align:center;
+display:inline-block;}
+a:hover, a:active{
+background-color:pink;}
+</style>
+</head>   
+   <section id="about">
+        <div class="content-box-md">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="vertical-heading">
+                            <h1>Who am I ?</h1>
+                            <h2> Let me talk about myself</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                                <i style="letter-spacing: 1px">
+                                <p>
+								I am Rama AlHafez from Syria, 25 years old, computer science student at UOPeople.
+								I am ambassador in Osboha 180 and one of the participants in web programming course, which started last year and finish last week, I was one of the successful participants in it, and I will be in programming team for programming Osboha 180 platform.</p>
+                                </p>
+                </div>
+          
+                </div>
+                <div id="progress-elements">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="col-md-12">
+                                <div class="Osboha courses">
+								<h2>Osboha courses:</h2>
+                                      <li><span>HTML 5 / CSS 5 / JavaScript</span></li>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="skill">
+                                     <li><span>php / Mysql </li></span>
+                                    </div>
+                                </div>
+								   </div>
+                            <div class="col-md-12">
+                                <div class="skill">
+                                    <li><span>Codeigniter </li></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="col-md-12">
+                                <div class="skill">
+                                     <li><span>Laravel </li></span>
+                                </div>
+                            </div>
+							<h1>Languages:</h1>
+							<li><i>English B2</i></li>
+							<li><i>Turkish A1</i></li>
+                            <li><i>Frensh A1</i></li>
+							
+                            <h1>Hoppies:</h1>
+                            <li><i>Reading</i></li>
+                              <li><i>Coding</i></li>
+                              <li><i>Drawing</i></li>
+							  <li><i>Photograph</i></li>
+ 
+                                <div id="offices">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="contact">
+											<h1>Contact:</h1>
+											
+                                                <li>
+                                                    <i class="fa fa-mobile"></i>
+                                                    <span>+963 948521648</span>
+                                                </li>
+                                                <li>
+                                                    <i class="fa fa-envelope"></i>
+                                                    <span>
+                                                        <a class="hint--bottom" href="mailto:prog.ramahafez@gmail.com" aria-label="Mesaj göndər" rel="noopener">prog.ramahafez@gmail.com</a>
+                                                    </span>
+                                                </li>
+                                                <li>
+                                                    <i class="fa fa-map-marker"></i>
+                                                    <span>Syria, Aleppo</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+								<h1>Social Media:</h1>
+								<a href = "https://www.facebook.com/" target="_blank" title="facebook">facebook</a><br><br>
+							    <a href = "https://www.instagram.com/" target="_blank" title="Instagram">Instagram</a>
+                            </div>
+</html>							
+            
+                                    
